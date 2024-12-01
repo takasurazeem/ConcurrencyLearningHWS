daf85ea87314adb7e7f6060bc097d38aae55633e
Takasur Azeem <takasurazeem@gmail.com>
2024-12-01 18:09:51 +0500

    Initial Commit

---
+++ ConcurrencyLearningHWS/ConcurrencyLearningHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyLearningHWSApp.swift
+//  ConcurrencyLearningHWS
+//
+//  Created by Takasur Azeem on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyLearningHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
